6be490bcc487eff666b71a44028927b952be1817
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-04-12 13:27:29 +0300

    Tenant API is responding unstaged added

--- src/test/java/org/folio/innreach/ModInnReachApplicationTests.java
+++ src/test/java/org/folio/innreach/ModInnReachApplicationTests.java
@@ -1,20 +1,7 @@
 package org.folio.innreach;
 
-import org.folio.innreach.domain.entity.ClientKey;
-import org.folio.innreach.repository.ClientKeyRepo;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 @SpringBootTest
 class ModInnReachApplicationTests {
-  @Autowired
-  private ClientKeyRepo clientKeyRepo;
-  @Test
-	void contextLoads() {
-    clientKeyRepo.save(new ClientKey(""+(new Date().getTime()),""+""+(new Date().getTime())));
-	}
-
 }
