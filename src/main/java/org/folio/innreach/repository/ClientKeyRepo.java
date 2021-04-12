a00a2b5dac865c0d3717a220e3fe202c1d4564e2
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-04-12 13:26:26 +0300

    Tenant API is responding

--- src/main/java/org/folio/innreach/repository/ClientKeyRepo.java
+++
@@ -1,8 +0,0 @@
-package org.folio.innreach.repository;
-
-
-import org.folio.innreach.domain.entity.ClientKey;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientKeyRepo extends CrudRepository<ClientKey,Long> {
-}
