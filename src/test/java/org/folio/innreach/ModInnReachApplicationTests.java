3d42a27fc23f024866361b38e5624dd05e4cc4d7
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-04-12 20:02:05 +0300

    fix

--- src/test/java/org/folio/innreach/ModInnReachApplicationTests.java
+++ src/test/java/org/folio/innreach/ModInnReachApplicationTests.java
@@ -4,14 +4,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ModInnReachApplicationTests {
-<<<<<<< HEAD
-  @Autowired
-  private ClientKeyRepo clientKeyRepo;
-//  @Test
-	void contextLoads() {
-    clientKeyRepo.save(new ClientKey(""+(new Date().getTime()),""+""+(new Date().getTime())));
-	}
 
-=======
->>>>>>> experimental
 }
