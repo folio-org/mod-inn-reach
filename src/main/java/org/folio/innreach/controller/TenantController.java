5247d04b7e8193d9e2fb8f1c05fb4138de6f4988
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-04-13 14:06:39 +0300

    fix

--- src/main/java/org/folio/innreach/controller/TenantController.java
+++ src/main/java/org/folio/innreach/controller/TenantController.java
@@ -1,31 +1,20 @@
 package org.folio.innreach.controller;
 
 import lombok.extern.log4j.Log4j2;
-import org.folio.spring.FolioExecutionContext;
+
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.folio.tenant.rest.resource.TenantApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Collections;
-import java.util.List;
+
 
 @Log4j2
 @RestController("folioTenantController")
 @RequestMapping(value = "/_/")
 public class TenantController implements TenantApi {
-
-  private static final String SAMPLES_DIR = "samples";
-  private final FolioExecutionContext context;
-  private final List<String> samples = Collections.singletonList("dematic.json");
-
-  @Autowired
-  public TenantController(FolioExecutionContext context) {
-    this.context = context;
-  }
 
   @Override
   public ResponseEntity<String> postTenant(@Valid TenantAttributes tenantAttributes) {
