a449bac87c5674a085aa6508dd604b996cec263a
Andrii Khodyka <andrii.khodyka@gmail.com>
2021-04-21 12:36:59 +0300

    MODINREACH-9 Add central_server_address column to central_server table

--- src/main/resources/db/changelog/changes/v1.0.0/sql/create-central-server-table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/create-central-server-table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE central_server
 (
-    id                SERIAL PRIMARY KEY,
-    name              VARCHAR(255) NOT NULL UNIQUE,
-    description       TEXT,
-    local_server_code VARCHAR(20)  NOT NULL UNIQUE
+    id                     SERIAL PRIMARY KEY,
+    name                   VARCHAR(255) NOT NULL UNIQUE,
+    description            TEXT,
+    local_server_code      VARCHAR(20)  NOT NULL UNIQUE,
+    central_server_address VARCHAR(255) NOT NULL
 );
