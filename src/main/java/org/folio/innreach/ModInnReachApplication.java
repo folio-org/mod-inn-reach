f17f735ff7d8bffc65e87262e7aff8fed7b70bda
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2021-05-13 13:41:30 +0300

    Remove custom TenantController; remove auto configuration exclusion from Application class

--- src/main/java/org/folio/innreach/ModInnReachApplication.java
+++ src/main/java/org/folio/innreach/ModInnReachApplication.java
@@ -2,16 +2,9 @@ package org.folio.innreach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = {
-  DataSourceAutoConfiguration.class,
-  HibernateJpaAutoConfiguration.class,
-  DataSourceTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableSwagger2
 public class ModInnReachApplication {
 
