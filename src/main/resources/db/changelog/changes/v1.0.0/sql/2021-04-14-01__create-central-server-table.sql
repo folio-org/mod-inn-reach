d0e82d1ae8f0e4f53c64d4c34f3d89e1e79f636c
Andrii Khodyka <andrii_khodyka@epam.com>
2021-05-17 12:31:26 +0300

    MODINREACH-17 Delete redundant foreign key from central_server table

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-04-14-01__create-central-server-table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-04-14-01__create-central-server-table.sql
@@ -5,6 +5,5 @@ CREATE TABLE central_server
     description            TEXT,
     local_server_code      VARCHAR(5)   NOT NULL UNIQUE,
     central_server_address VARCHAR(255) NOT NULL,
-    loan_type_id           VARCHAR(255) NOT NULL,
-    location_id            UUID         NOT NULL
+    loan_type_id           VARCHAR(255) NOT NULL
 );
