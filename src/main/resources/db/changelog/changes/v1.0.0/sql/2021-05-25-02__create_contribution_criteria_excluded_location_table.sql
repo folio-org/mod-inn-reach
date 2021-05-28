519550f01e994bb64aaa2031fe91d4a2785420fc
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-05-28 08:26:37 +0300

    fix:db creation script, extra config file removed

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-02__create_contribution_criteria_excluded_location_table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-02__create_contribution_criteria_excluded_location_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE public.contribution_criteria_excluded_location
     excluded_location_id uuid,
     criteria_configuration_id uuid,
     CONSTRAINT contribution_criteria_excluded_location_pkey PRIMARY KEY (id),
-    CONSTRAINT fkoi7sg96sfu88or7jcs99po0ca FOREIGN KEY (criteria_configuration_id)
+    CONSTRAINT criteria_configuration_el_fk0 FOREIGN KEY (criteria_configuration_id)
         REFERENCES public.contribution_criteria_configuration (central_server_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-03__create_statistical_code_behavior_table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-03__create_statistical_code_behavior_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE public.statistical_code_behavior
 (
     id UUID NOT NULL,
-    contribution_behavior CHARACTER VARYING(50),
+    contribution_behavior VARCHAR(30),
     statistical_code_id UUID,
     criteria_configuration_id UUID,
     CONSTRAINT statistical_code_behavior_pkey PRIMARY KEY (id),
-    CONSTRAINT fktc5rm9nccoo6u25cirqtk60hc FOREIGN KEY (criteria_configuration_id)
+    CONSTRAINT criteria_configuration_scb_fk0 FOREIGN KEY (criteria_configuration_id)
         REFERENCES public.contribution_criteria_configuration (central_server_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
