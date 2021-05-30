5b02c241e013f0a01065d1d0e80e1cebc1719783
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-05-30 15:19:53 +0300

    stress  test

--- src/test/java/org/folio/innreach/repository/ContributionCriteriaConfigurationRepositoryTest.java
+++ src/test/java/org/folio/innreach/repository/ContributionCriteriaConfigurationRepositoryTest.java
@@ -1,18 +1,19 @@
 package org.folio.innreach.repository;
 
+import liquibase.pro.packaged.D;
 import org.folio.innreach.domain.entity.ContributionCriteriaConfiguration;
 import org.folio.innreach.domain.entity.ContributionCriteriaExcludedLocation;
 import org.folio.innreach.domain.entity.ContributionCriteriaStatisticalCodeBehavior;
-import org.folio.innreach.fixture.CentralServerFixture;
 import org.folio.innreach.fixture.ContributionCriteriaConfigurationFixture;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
-import java.util.Set;
+import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -26,6 +27,24 @@ class ContributionCriteriaConfigurationRepositoryTest extends BaseRepositoryTest
   private static String CENTRAL_SERVER_ID = "edab6baf-c696-42b1-89bb-1bbb8759b0d2";
   private static final List<UUID> UUUID_IDs = List.of(UUID.randomUUID(),UUID.randomUUID(),UUID.randomUUID());
 
+
+  private static int TEST_DEFINED_QUANTITY_OF_CONFIGURATIONS = 100;
+
+  private static List<UUID> TEST_DEFINED_CONFIGURATION_IDs = new ArrayList<>(TEST_DEFINED_QUANTITY_OF_CONFIGURATIONS);
+
+  private static List<UUID> TEST_DEFINED_LOCATION_UUIDs = new ArrayList<>();
+
+  private static int TEST_DEFINED_QUATITY_OF_LOCATIONS = 200;
+
+  private static int TEST_DEFINED_QUANTITY_OF_LOCATION_SETS = 50;
+
+  private static int TEST_DEFINED_MAX_SIZE_OF_LOCATION_SET = 50;
+
+  private static List<List<ContributionCriteriaExcludedLocation>> TEST_DEFINED_LOCATIONS_SETs = new ArrayList<>(TEST_DEFINED_QUANTITY_OF_LOCATION_SETS);
+
+  private static Random random = new Random();
+
+
   @Autowired
   private CentralServerRepository centralServerRepository;
 
@@ -35,7 +54,32 @@ class ContributionCriteriaConfigurationRepositoryTest extends BaseRepositoryTest
   @Autowired
   private ContributionCriteriaConfigurationRepository contributionCriteriaConfigurationRepository;
 
-  
+  @BeforeAll
+  static void init() {
+
+    // ContributionConfiguration UUIDs
+    for (int i = 0; i < TEST_DEFINED_QUANTITY_OF_CONFIGURATIONS; i++) {
+      TEST_DEFINED_CONFIGURATION_IDs.add(UUID.randomUUID());
+    }
+
+    // Location UUIDs
+    for (int i = 0; i < TEST_DEFINED_QUATITY_OF_LOCATIONS; i++) {
+      TEST_DEFINED_LOCATION_UUIDs.add(UUID.randomUUID());
+    }
+
+    // location sets
+    for (int i = 0; i < TEST_DEFINED_QUANTITY_OF_LOCATION_SETS; i++) {
+      List<ContributionCriteriaExcludedLocation> locationSet = new ArrayList<>();
+
+      for (int j = 0; j < myRandom(3,TEST_DEFINED_MAX_SIZE_OF_LOCATION_SET); j++) {
+        ContributionCriteriaExcludedLocation exLocation = new ContributionCriteriaExcludedLocation();
+        exLocation.setExcludedLocationId(TEST_DEFINED_LOCATION_UUIDs.get(myRandom(0,TEST_DEFINED_QUATITY_OF_LOCATIONS)));
+        locationSet.add(exLocation);
+      }
+      TEST_DEFINED_LOCATIONS_SETs.add(locationSet);
+    }
+  }
+
   @BeforeEach
   void beforeEach() {
     ContributionCriteriaConfiguration criteriaConfiguration
@@ -43,6 +87,56 @@ class ContributionCriteriaConfigurationRepositoryTest extends BaseRepositoryTest
       .createTestContributionCriteriaConfiguration(UUID.fromString(CENTRAL_SERVER_ID));
     contributionCriteriaConfigurationRepository.saveAndFlush(criteriaConfiguration);
   }
+
+  @Test
+  void updateContributionCriteriaExcludedLocationsStressTest() {
+    Long startTime = new Date().getTime();
+    for (int i = 0; i < TEST_DEFINED_QUANTITY_OF_CONFIGURATIONS; i++) {
+      ContributionCriteriaConfiguration configuration = new ContributionCriteriaConfiguration();
+      configuration.setCentralServeId(TEST_DEFINED_CONFIGURATION_IDs.get(i));
+      int randomLocationSetIndex = (int) Math.random()*TEST_DEFINED_QUANTITY_OF_LOCATION_SETS;
+      List<ContributionCriteriaExcludedLocation> excludedLocations = randomGetTestPredefinedLocationList();
+      configuration.updateExcludedLocations(excludedLocations);
+      contributionCriteriaConfigurationRepository.saveAndFlush(configuration);
+      var optConfig = contributionCriteriaConfigurationRepository.findById(configuration.getCentralServeId());
+      assertTrue(optConfig.isPresent());
+      assertEquals(0,differenceBetveenExcludedLocationsCollections(excludedLocations,
+        optConfig.get().getExcludedLocations().stream().collect(Collectors.toList())).size());
+    }
+    for (int i = 0; i < 1000; i++) {
+      var optConfig
+        = contributionCriteriaConfigurationRepository.findById(TEST_DEFINED_CONFIGURATION_IDs.get((int)(Math.random()*TEST_DEFINED_QUANTITY_OF_CONFIGURATIONS)));
+      assertTrue(optConfig.isPresent());
+      var excludedLocations = randomGetTestPredefinedLocationList();
+      optConfig.get().updateExcludedLocations(excludedLocations);
+      optConfig
+        = contributionCriteriaConfigurationRepository.findById(optConfig.get().getCentralServeId());
+      assertEquals(0,differenceBetveenExcludedLocationsCollections(excludedLocations,
+        optConfig.get().getExcludedLocations().stream().collect(Collectors.toList())).size());
+    }
+    System.out.println(("Test time:"+(new Date().getTime()-startTime)/1000));
+  }
+
+  private static int myRandom(int min, int max) {
+//    String tString = "" + new Date().getTime();
+    Double dr = random.nextGaussian();
+    String tString = String.valueOf(dr);
+    if (tString.contains("E")) tString = tString.substring(0,tString.indexOf("E"));
+    int len = (""+max).length();
+    String sValue = tString.substring(tString.length()-len);
+    int result = Integer.parseInt(sValue);
+    while (result>max-1) result = result + min - max/2;
+    if (result<min) result = result + min;
+    return result;
+  }
+
+  private List<ContributionCriteriaExcludedLocation> randomGetTestPredefinedLocationList() {
+    int randomIndexOfLocationSet = myRandom(0,TEST_DEFINED_QUANTITY_OF_LOCATION_SETS);
+    return TEST_DEFINED_LOCATIONS_SETs.get(randomIndexOfLocationSet);
+  }
+
+
+
 
   @Test
   void chekIfContributionCriteriaConfigurationCreated() {
