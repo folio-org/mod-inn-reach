a5735490fcbe74278903864be41c02cce40f4588
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-06-01 11:24:34 +0300

    Remove DDL operands duplicates default.

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-02__create_contribution_criteria_excluded_location_table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-02__create_contribution_criteria_excluded_location_table.sql
@@ -6,6 +6,4 @@ CREATE TABLE public.contribution_criteria_excluded_location
     CONSTRAINT contribution_criteria_excluded_location_pkey PRIMARY KEY (id),
     CONSTRAINT fk_excluded_location__criteria_configuration FOREIGN KEY (criteria_configuration_id)
         REFERENCES contribution_criteria_configuration (central_server_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
 );
--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-03__create_statistical_code_behavior_table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-05-25-03__create_statistical_code_behavior_table.sql
@@ -7,6 +7,4 @@ CREATE TABLE public.statistical_code_behavior
     CONSTRAINT statistical_code_behavior_pkey PRIMARY KEY (id),
     CONSTRAINT fk_statistical_code_behavior__criteria_configuration FOREIGN KEY (criteria_configuration_id)
         REFERENCES contribution_criteria_configuration (central_server_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
 );
