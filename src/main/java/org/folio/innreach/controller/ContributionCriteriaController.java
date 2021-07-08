b26b6a06d1b18d78e9a2037ced1efe36761764c4
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2021-07-08 17:06:34 +0300

    update API spec and controller

--- src/main/java/org/folio/innreach/controller/ContributionCriteriaController.java
+++ src/main/java/org/folio/innreach/controller/ContributionCriteriaController.java
@@ -2,18 +2,15 @@ package org.folio.innreach.controller;
 
 import java.util.UUID;
 
-import javax.validation.Valid;
-
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,44 +18,45 @@ import org.folio.innreach.domain.service.ContributionCriteriaConfigurationServic
 import org.folio.innreach.dto.ContributionCriteriaDTO;
 import org.folio.innreach.rest.resource.ContributionCriteriaApi;
 
-@Log4j2
 @RequiredArgsConstructor
 @RestController
+@Validated
 @RequestMapping("/inn-reach/central-servers/")
 public class ContributionCriteriaController implements ContributionCriteriaApi {
 
-  private final ContributionCriteriaConfigurationService criteriaConfigurationService;
+  private final ContributionCriteriaConfigurationService service;
 
 
   @Override
-  @PostMapping("/contribution-criteria")
-  public ResponseEntity<ContributionCriteriaDTO> postContributionCriteria(
-    @Valid @RequestBody ContributionCriteriaDTO contributionCriteriaDTO) {
+  @GetMapping("/{centralServerId}/contribution-criteria")
+  public ResponseEntity<ContributionCriteriaDTO> getCriteriaByServerId(@PathVariable UUID centralServerId) {
+    var criteria = service.getCriteria(centralServerId);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(criteriaConfigurationService
-      .createCriteria(contributionCriteriaDTO));
+    return ResponseEntity.ok(criteria);
+  }
+
+  @Override
+  @PostMapping("/{centralServerId}/contribution-criteria")
+  public ResponseEntity<ContributionCriteriaDTO> postContributionCriteria(@PathVariable UUID centralServerId,
+      ContributionCriteriaDTO dto) {
+    var criteria = service.createCriteria(centralServerId, dto);
+    
+    return ResponseEntity.status(HttpStatus.CREATED).body(criteria);
+  }
+
+  @Override
+  @PutMapping("/{centralServerId}/contribution-criteria")
+  public ResponseEntity<Void> updateCriteria(@PathVariable UUID centralServerId, ContributionCriteriaDTO dto) {
+    service.updateCriteria(centralServerId, dto);
+
+    return ResponseEntity.noContent().build();
   }
 
   @Override
   @DeleteMapping("/{centralServerId}/contribution-criteria")
   public ResponseEntity<Void> deleteCriteria(@PathVariable UUID centralServerId) {
-    criteriaConfigurationService.deleteCriteria(centralServerId);
-    return ResponseEntity.noContent().build();
-  }
-
-  @Override
-  @GetMapping("/{centralServerId}/contribution-criteria")
-  public ResponseEntity<ContributionCriteriaDTO> getCriteriaById(@PathVariable UUID centralServerId) {
-    return ResponseEntity.ok(
-      criteriaConfigurationService.getCriteria(centralServerId)
-    );
-  }
-
-  @Override
-  @PutMapping("/{centralServerId}/contribution-criteria")
-  public ResponseEntity<Void> updateCriteria(@PathVariable UUID centralServerId,
-                                                                @Valid ContributionCriteriaDTO contributionCriteriaDTO) {
-    criteriaConfigurationService.updateCriteria(contributionCriteriaDTO);
+    service.deleteCriteria(centralServerId);
+    
     return ResponseEntity.noContent().build();
   }
 
