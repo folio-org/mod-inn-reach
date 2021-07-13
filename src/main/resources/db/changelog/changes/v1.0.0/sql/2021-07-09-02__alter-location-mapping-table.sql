d38e4fe34ecd0e9abc2d6a20f32d4672a0db606f
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2021-07-13 12:24:17 +0300

    add missing cascade rule to fk_location_mapping_ir_location

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-07-09-02__alter-location-mapping-table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-07-09-02__alter-location-mapping-table.sql
@@ -5,3 +5,11 @@ ALTER TABLE location_mapping
     ADD CONSTRAINT fk_location_mapping_central_server FOREIGN KEY (central_server_id)
         REFERENCES central_server (id)
         ON DELETE CASCADE;
+
+ALTER TABLE location_mapping
+    DROP CONSTRAINT fk_location_mapping_ir_location;
+
+ALTER TABLE location_mapping
+    ADD CONSTRAINT fk_location_mapping_ir_location FOREIGN KEY (ir_location_id)
+        REFERENCES inn_reach_location (id)
+        ON DELETE CASCADE;
