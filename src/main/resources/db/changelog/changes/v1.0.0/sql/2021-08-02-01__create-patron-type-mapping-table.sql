e8d2ddddc0f77cdcbb87392432704b85eea7dad3
Maryna_Steshenko <Maryna_Steshenko@epam.com>
2021-08-04 15:04:47 +0300

    Comments fix

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-08-02-01__create-patron-type-mapping-table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-08-02-01__create-patron-type-mapping-table.sql
@@ -8,7 +8,6 @@ CREATE TABLE patron_type_mapping
     created_date TIMESTAMP,
     last_modified_by VARCHAR(255),
     last_modified_date TIMESTAMP,
-    update_counter INTEGER,
     CONSTRAINT pk_patron_type_mapping PRIMARY KEY (id),
     CONSTRAINT fk_patron_type_mapping_central_server FOREIGN KEY (central_server_id)
     REFERENCES central_server (id) ON DELETE CASCADE
