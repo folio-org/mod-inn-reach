a2d95dd4b3eb307e111b523d2bfb0fc26ffd9aa1
Andrii Khodyka <andrii_khodyka@epam.com>
2021-09-08 15:40:23 +0300

    MODINREACH-76 Update metadata columns

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-09-01-01__create_central_patron_type_mapping_table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-09-01-01__create_central_patron_type_mapping_table.sql
@@ -4,10 +4,12 @@ CREATE TABLE central_patron_type_mapping
     central_patron_type INT          NOT NULL CHECK (central_patron_type BETWEEN 0 AND 255),
     folio_user_barcode  VARCHAR(255) NOT NULL,
     central_server_id   UUID         NOT NULL,
-    created_by          VARCHAR(255) NOT NULL,
-    created_date        TIMESTAMP    NOT NULL,
-    last_modified_by    VARCHAR(255),
-    last_modified_date  TIMESTAMP,
+    created_by_userid   UUID         NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
+    created_by_username VARCHAR(255) NOT NULL DEFAULT 'SYSTEM',
+    updated_by_userid   UUID,
+    updated_by_username VARCHAR(255),
+    created_date        TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_date        TIMESTAMP,
     CONSTRAINT pk_central_patron_type_mapping PRIMARY KEY (id),
     CONSTRAINT fk_central_patron_type_mapping_central_server FOREIGN KEY (central_server_id)
         REFERENCES central_server (id) ON DELETE CASCADE
