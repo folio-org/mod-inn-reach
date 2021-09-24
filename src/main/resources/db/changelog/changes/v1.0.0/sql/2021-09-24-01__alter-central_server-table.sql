536d316dd9cfaf2c3f10a03878d59a831735adc6
Oleksandr Oliinyk <86827432+oliinyko@users.noreply.github.com>
2021-09-24 12:55:52 +0300

    Update alter sql. Add centralServerCode to test data.

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-09-24-01__alter-central_server-table.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-09-24-01__alter-central_server-table.sql
@@ -1,2 +1,7 @@
 ALTER TABLE central_server
-  ADD COLUMN central_server_code VARCHAR(5) NOT NULL DEFAULT 'd2ir';
+  ADD COLUMN central_server_code VARCHAR(5) NULL;
+
+UPDATE central_server SET central_server_code = 'dri2';
+
+ALTER TABLE central_server
+  ALTER COLUMN central_server_code VARCHAR(5) NOT NULL;
