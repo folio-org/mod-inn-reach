1d6353cd8bfd78b01d8be38c0dde9e8f22c957e3
Oleksandr Oliinyk <86827432+oliinyko@users.noreply.github.com>
2021-10-08 11:40:26 +0300

    Add check for unknown event

--- src/main/java/org/folio/innreach/batch/contribution/service/InstanceLoader.java
+++ src/main/java/org/folio/innreach/batch/contribution/service/InstanceLoader.java
@@ -2,11 +2,15 @@ package org.folio.innreach.batch.contribution.service;
 
 import static org.folio.innreach.domain.service.impl.MARCRecordTransformationServiceImpl.isMARCRecord;
 
+import java.util.Objects;
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Service;
 
+import org.folio.innreach.batch.contribution.ContributionJobContextManager;
 import org.folio.innreach.domain.dto.folio.inventorystorage.InstanceIterationEvent;
 import org.folio.innreach.domain.service.InventoryViewService;
 import org.folio.innreach.dto.Instance;
@@ -19,14 +23,19 @@ public class InstanceLoader implements ItemProcessor<InstanceIterationEvent, Ins
   private final InventoryViewService inventoryService;
 
   @Override
-  public Instance process(InstanceIterationEvent instanceIterationEvent) throws Exception {
-    log.info("Processing instance iteration event = {}", instanceIterationEvent);
+  public Instance process(InstanceIterationEvent event) throws Exception {
+    log.info("Processing instance iteration event = {}", event);
+
+    if (isUnknownEvent(event)) {
+      log.info("Skipping unknown event of job {}, current job is {}", event.getJobId(), getIterationJobId());
+      return null;
+    }
 
     // if the returned instance is null it is assumed that processing of the event should not continue
-    var instance = inventoryService.getInstance(instanceIterationEvent.getInstanceId());
+    var instance = inventoryService.getInstance(event.getInstanceId());
 
     if (instance == null) {
-      log.info("No instance found by id {}", instanceIterationEvent.getInstanceId());
+      log.info("No instance found by id {}", event.getInstanceId());
       return null;
     }
 
@@ -38,6 +47,15 @@ public class InstanceLoader implements ItemProcessor<InstanceIterationEvent, Ins
     log.info("Loaded instance with hrid {}", instance.getHrid());
 
     return instance;
+  }
+
+  private boolean isUnknownEvent(InstanceIterationEvent event) {
+    var iterationJobId = getIterationJobId();
+    return !Objects.equals(event.getJobId(), iterationJobId);
+  }
+
+  private UUID getIterationJobId() {
+    return ContributionJobContextManager.getContributionJobContext().getIterationJobId();
   }
 
 }
