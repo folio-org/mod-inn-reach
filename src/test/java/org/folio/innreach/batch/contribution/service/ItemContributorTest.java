787de994478b6c8386bdbe6bed123d8ef81c68db
Oleksandr Oliinyk <86827432+oliinyko@users.noreply.github.com>
2021-10-08 14:43:23 +0300

    Fix test

--- src/test/java/org/folio/innreach/batch/contribution/service/ItemContributorTest.java
+++ src/test/java/org/folio/innreach/batch/contribution/service/ItemContributorTest.java
@@ -1,5 +1,6 @@
 package org.folio.innreach.batch.contribution.service;
 
+import static com.google.common.collect.ImmutableList.of;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
@@ -7,18 +8,19 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import static org.folio.innreach.external.dto.InnReachResponse.okResponse;
-import static org.folio.innreach.fixture.FolioContextFixture.createTenantExecutionService;
+import static org.folio.innreach.fixture.ContributionFixture.createContributionJobContext;
+import static org.folio.innreach.fixture.ContributionFixture.createItem;
 
-import java.util.UUID;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.folio.innreach.batch.contribution.ContributionJobContext;
+import org.folio.innreach.batch.contribution.ContributionJobContextManager;
 import org.folio.innreach.batch.contribution.listener.ContributionExceptionListener;
 import org.folio.innreach.domain.dto.folio.ContributionItemCirculationStatus;
 import org.folio.innreach.domain.service.CentralServerService;
@@ -27,7 +29,6 @@ import org.folio.innreach.domain.service.InnReachLocationService;
 import org.folio.innreach.domain.service.LibraryMappingService;
 import org.folio.innreach.domain.service.LocationMappingService;
 import org.folio.innreach.domain.service.MaterialTypeMappingService;
-import org.folio.innreach.domain.service.impl.TenantScopedExecutionService;
 import org.folio.innreach.dto.CentralServerDTO;
 import org.folio.innreach.dto.InnReachLocationsDTO;
 import org.folio.innreach.dto.LibraryMappingsDTO;
@@ -37,7 +38,7 @@ import org.folio.innreach.external.service.InnReachContributionService;
 @ExtendWith(MockitoExtension.class)
 class ItemContributorTest {
 
-  private static final UUID CENTRAL_SERVER_ID = UUID.randomUUID();
+  private static final ContributionJobContext JOB_CONTEXT = createContributionJobContext();
 
   @Mock
   private InnReachContributionService irContributionService;
@@ -54,19 +55,23 @@ class ItemContributorTest {
   @Mock
   private LocationMappingService locationMappingService;
   @Mock
-  private ContributionJobContext jobContext;
-  @Mock
   private ContributionExceptionListener exceptionListener;
-
-  @Spy
-  private TenantScopedExecutionService tenantScopedExecutionService = createTenantExecutionService();
 
   @InjectMocks
   private ItemContributor service;
 
+  @BeforeEach
+  public void init() {
+    ContributionJobContextManager.beginContributionJobContext(JOB_CONTEXT);
+  }
+
+  @AfterEach
+  public void clear() {
+    ContributionJobContextManager.endContributionJobContext();
+  }
+
   @Test
   void shouldContributeItems() {
-    when(jobContext.getCentralServerId()).thenReturn(CENTRAL_SERVER_ID);
     when(irContributionService.contributeBibItems(any(), any(), any())).thenReturn(okResponse());
     when(irLocationService.getAllInnReachLocations(any(), any())).thenReturn(new InnReachLocationsDTO());
     when(typeMappingService.getAllMappings(any(), anyInt(), anyInt())).thenReturn(new MaterialTypeMappingsDTO());
@@ -74,7 +79,9 @@ class ItemContributorTest {
     when(centralServerService.getCentralServer(any())).thenReturn(new CentralServerDTO());
     when(validationService.getItemCirculationStatus(any(), any())).thenReturn(ContributionItemCirculationStatus.AVAILABLE);
 
-    verify(irContributionService).contributeBibItems(eq(CENTRAL_SERVER_ID), any(), any());
+    service.contributeItems("test", of(createItem()));
+
+    verify(irContributionService).contributeBibItems(eq(JOB_CONTEXT.getCentralServerId()), any(), any());
   }
 
 }
