028d43fb7fa6396d1454ba351f5a5899d6711396
Oleksandr Oliinyk <86827432+oliinyko@users.noreply.github.com>
2021-10-18 12:23:27 +0300

    Add unique constraint

--- src/main/resources/db/changelog/changes/v1.0.0/sql/2021-10-18-01__alter-user-custom-field-mapping-tables.sql
+++ src/main/resources/db/changelog/changes/v1.0.0/sql/2021-10-18-01__alter-user-custom-field-mapping-tables.sql
@@ -2,3 +2,6 @@ DELETE FROM user_custom_field_mapping;
 
 ALTER TABLE user_custom_field_mapping
   ALTER COLUMN custom_field_id TYPE VARCHAR(256);
+
+ALTER TABLE user_custom_field_configured_options
+  ADD CONSTRAINT unq_user_custom_field_options_value UNIQUE (user_custom_field_mapping_id, custom_field_value)
