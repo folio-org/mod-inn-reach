11bab34f6826a715cb13f8009cdfaf2446b8ffb7
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2021-11-04 21:15:09 +0200

    interface definition

--- src/main/java/org/folio/innreach/controller/d2ir/InnReachCirculationController.java
+++ src/main/java/org/folio/innreach/controller/d2ir/InnReachCirculationController.java
@@ -4,8 +4,10 @@ import javax.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.folio.innreach.domain.service.CirculationService;
 import org.folio.innreach.dto.InnReachResponseDTO;
 import org.folio.innreach.dto.TransactionHoldDTO;
+import org.folio.innreach.dto.TransferRequestDTO;
 import org.folio.innreach.rest.resource.InnReachCirculationApi;
 
 @RestController
+@Validated
 @RequestMapping("/inn-reach/d2ir/circ")
 @RequiredArgsConstructor
 public class InnReachCirculationController implements InnReachCirculationApi {
@@ -31,4 +35,12 @@ public class InnReachCirculationController implements InnReachCirculationApi {
     var innReachResponse = circulationService.processCirculationRequest(trackingId, centralCode, circulationOperationName, transactionHold);
     return ResponseEntity.ok(innReachResponse);
   }
+
+  @Override
+  @PutMapping("/transferrequest/{trackingId}/{centralCode}")
+  public ResponseEntity<InnReachResponseDTO> putTransferRequest(@PathVariable String trackingId,
+      @PathVariable String centralCode, TransferRequestDTO transferRequestDTO) {
+    return InnReachCirculationApi.super.putTransferRequest(trackingId, centralCode, transferRequestDTO);
+  }
+
 }
