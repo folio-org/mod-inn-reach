35869ee2b55bfdfe33f005292eea0979b73ead62
Oleksandr Oliinyk <86827432+oliinyko@users.noreply.github.com>
2021-11-05 12:05:22 +0200

    Fix warnings

--- src/main/java/org/folio/innreach/domain/processor/PatronHoldCancelRequestProcessor.java
+++ src/main/java/org/folio/innreach/domain/processor/PatronHoldCancelRequestProcessor.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
+import org.folio.innreach.domain.entity.InnReachTransaction;
+import org.folio.innreach.domain.exception.EntityNotFoundException;
 import org.folio.innreach.domain.service.InventoryStorageService;
 import org.folio.innreach.domain.service.RequestService;
 import org.folio.innreach.dto.CirculationRequestDTO;
@@ -32,7 +34,8 @@ public class PatronHoldCancelRequestProcessor implements InnReachCirculationProc
 
   @Override
   public InnReachResponseDTO process(String trackingId, String centralCode, CirculationRequestDTO request) {
-    var transaction = transactionRepository.fetchOneByTrackingId(trackingId).get();
+    var transaction = getInnReachTransaction(trackingId);
+
     var hold = transaction.getHold();
     var itemId = hold.getFolioItemId();
 
@@ -46,6 +49,12 @@ public class PatronHoldCancelRequestProcessor implements InnReachCirculationProc
       .ifPresent(inventoryStorageService::updateHolding);
 
     return new InnReachResponseDTO().status("ok").reason("success");
+  }
+
+  private InnReachTransaction getInnReachTransaction(String trackingId) {
+    return transactionRepository.fetchOneByTrackingId(trackingId)
+      .orElseThrow(() -> new EntityNotFoundException(
+        String.format("INN-Reach transaction with trackingId [%s] not found", trackingId)));
   }
 
   private Function<Holding, Holding> removeHoldingTransactionInfo() {
