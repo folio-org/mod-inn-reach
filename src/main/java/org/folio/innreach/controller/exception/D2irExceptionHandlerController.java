282d86449b51e49e847cab6fe6ca8b5183dc8724
Oleksandr Oliinyk <86827432+oliinyko@users.noreply.github.com>
2021-12-17 18:23:02 +0200

    Enhance D2ir response processing. (#159)

--- src/main/java/org/folio/innreach/controller/exception/D2irExceptionHandlerController.java
+++ src/main/java/org/folio/innreach/controller/exception/D2irExceptionHandlerController.java
@@ -1,9 +1,12 @@
 package org.folio.innreach.controller.exception;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +18,7 @@ import org.folio.innreach.domain.exception.EntityNotFoundException;
 import org.folio.innreach.dto.InnReachResponseDTO;
 import org.folio.innreach.mapper.InnReachErrorMapper;
 
+@Order(HIGHEST_PRECEDENCE)
 @Log4j2
 @RequiredArgsConstructor
 @RestControllerAdvice(basePackages = "org.folio.innreach.controller.d2ir")
--- src/main/java/org/folio/innreach/external/client/feign/error/InnReachFeignErrorDecoder.java
+++ src/main/java/org/folio/innreach/external/client/feign/error/InnReachFeignErrorDecoder.java
@@ -1,5 +1,7 @@
 package org.folio.innreach.external.client.feign.error;
 
+import static feign.FeignException.errorStatus;
+
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.log4j.Log4j2;
@@ -9,16 +11,16 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.folio.innreach.external.exception.InnReachException;
 
 @Log4j2
-public class FeignErrorDecoder implements ErrorDecoder {
+public class InnReachFeignErrorDecoder implements ErrorDecoder {
 
   @Override
-  public Exception decode(String s, Response response) {
+  public Exception decode(String methodKey, Response response) {
     if (HttpStatus.valueOf(response.status()).equals(HttpStatus.UNAUTHORIZED)) {
       log.debug("Can't get InnReach access token. CentralServer authentication failed with status: {}", response.status());
       return new BadCredentialsException("Can't get InnReach access token. Key/Secret pair is not valid");
     }
 
-    log.debug("Inn Reach call failed with status: {}", response.status());
-    return new InnReachException("Inn Reach call failed with status: " + response.status());
+    var e = errorStatus(methodKey, response);
+    return new InnReachException("INN-Reach call failed: " + e.getMessage());
   }
 }
