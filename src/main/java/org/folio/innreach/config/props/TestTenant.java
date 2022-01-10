27f009ae961130dde7850f137d00bb3fd817be1f
Oleksandr_Oliinyk2 <oleksandr_oliinyk2@epam.com>
2022-01-10 21:39:48 +0200

    mod-inn-reach: Setup API Karate tests in the module. Fixed TestTenant.

--- src/main/java/org/folio/innreach/config/props/TestTenant.java
+++ src/main/java/org/folio/innreach/config/props/TestTenant.java
@@ -3,9 +3,11 @@ package org.folio.innreach.config.props;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
 @ConfigurationProperties("test-tenant")
+@Component
 public class TestTenant {
   private String tenantName;
 }
