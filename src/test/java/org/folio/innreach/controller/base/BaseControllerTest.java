93d0cf879b850ff55c60740c6ea35dcae24e2416
Oleksandr Oliinyk <oleksandr_oliinyk1@epam.com>
2022-01-19 19:46:39 +0200

    Mock kafka listener in API tests

--- src/test/java/org/folio/innreach/controller/base/BaseControllerTest.java
+++ src/test/java/org/folio/innreach/controller/base/BaseControllerTest.java
@@ -4,29 +4,32 @@ import javax.validation.Valid;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.folio.innreach.ModInnReachApplication;
+import org.folio.innreach.domain.listener.KafkaCirculationEventListener;
 import org.folio.spring.liquibase.FolioLiquibaseConfiguration;
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.folio.tenant.rest.resource.TenantApi;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { ModInnReachApplication.class,
-	BaseControllerTest.TestTenantController.class })
-@ActiveProfiles({ "test", "testcontainers-pg" })
+@MockBean(KafkaCirculationEventListener.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ModInnReachApplication.class,
+  BaseControllerTest.TestTenantController.class})
+@ActiveProfiles({"test", "testcontainers-pg"})
 public class BaseControllerTest {
 
-    @EnableAutoConfiguration(exclude = { FolioLiquibaseConfiguration.class })
-    @RestController("folioTenantController")
-    @Profile("test")
-    static class TestTenantController implements TenantApi {
+  @EnableAutoConfiguration(exclude = {FolioLiquibaseConfiguration.class})
+  @RestController("folioTenantController")
+  @Profile("test")
+  static class TestTenantController implements TenantApi {
 
-      @Override
-      public ResponseEntity<String> postTenant(@Valid TenantAttributes tenantAttributes) {
-          return ResponseEntity.ok("OK");
-      }
+    @Override
+    public ResponseEntity<String> postTenant(@Valid TenantAttributes tenantAttributes) {
+      return ResponseEntity.ok("OK");
     }
+  }
 }
