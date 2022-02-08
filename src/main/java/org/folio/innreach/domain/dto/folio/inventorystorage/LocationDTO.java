f3154277d5fe640a6332b18fc66b849418e3e100
Oleksandr Oliinyk <oleksandr_oliinyk1@epam.com>
2022-02-08 19:00:18 +0200

    Add folio location service test

--- src/main/java/org/folio/innreach/domain/dto/folio/inventorystorage/LocationDTO.java
+++ src/main/java/org/folio/innreach/domain/dto/folio/inventorystorage/LocationDTO.java
@@ -2,9 +2,13 @@ package org.folio.innreach.domain.dto.folio.inventorystorage;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LocationDTO {
   private UUID id;
   private UUID libraryId;
