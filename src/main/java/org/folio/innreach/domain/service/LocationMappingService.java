7b823b6ed61396a6ea124b96d59e0b8bf26424f7
Alherd Nestsiarkow <76254623+s1mpleGit@users.noreply.github.com>
2022-03-10 12:32:36 +0300

    MODINREACH-227 - Create API endpoint (GET) for location mappings (#207)
    * fix response type
    
    * fix response type
    
    * test
    
    * test

--- src/main/java/org/folio/innreach/domain/service/LocationMappingService.java
+++ src/main/java/org/folio/innreach/domain/service/LocationMappingService.java
@@ -1,14 +1,16 @@
 package org.folio.innreach.domain.service;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.folio.innreach.dto.LocationMappingDTO;
 import org.folio.innreach.dto.LocationMappingsDTO;
 
 public interface LocationMappingService {
 
   LocationMappingsDTO getMappingsByLibraryId(UUID centralServerId, UUID libraryId, int offset, int limit);
 
-  LocationMappingsDTO getAllMappings(UUID centralServerId, int offset, int limit);
+  List<LocationMappingDTO> getAllMappings(UUID centralServerId);
 
   LocationMappingsDTO updateAllMappings(UUID centralServerId, UUID libraryId, LocationMappingsDTO locationMappingsDTO);
 
