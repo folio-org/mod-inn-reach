6636a5f3bd2f301a25881bebd9320574d1537725
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2022-03-15 17:48:43 +0200

    transaction/hold audit field testing

--- src/test/java/org/folio/innreach/controller/InnReachTransactionDateFilterTest.java
+++ src/test/java/org/folio/innreach/controller/InnReachTransactionDateFilterTest.java
@@ -21,11 +21,11 @@ import static org.folio.innreach.util.ListUtils.mapItems;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -36,11 +36,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 
 import org.folio.innreach.controller.base.BaseControllerTest;
-import org.folio.innreach.domain.entity.InnReachTransaction;
 import org.folio.innreach.dto.FilterDateOperation;
 import org.folio.innreach.dto.InnReachTransactionDTO;
 import org.folio.innreach.dto.InnReachTransactionsDTO;
-import org.folio.innreach.repository.InnReachTransactionRepository;
 
 @Sql(
     scripts = {
@@ -56,7 +54,7 @@ import org.folio.innreach.repository.InnReachTransactionRepository;
     executionPhase = AFTER_TEST_METHOD
 )
 @SqlMergeMode(MERGE)
-public class InnReachTransactionDateFilterTest extends BaseControllerTest {
+class InnReachTransactionDateFilterTest extends BaseControllerTest {
 
   private static final String PRE_POPULATED_TRACKING_ID1 = "tracking1";
   private static final String PRE_POPULATED_TRACKING_ID2 = "tracking2";
@@ -68,91 +66,152 @@ public class InnReachTransactionDateFilterTest extends BaseControllerTest {
 
   @Autowired
   private TestRestTemplate testRestTemplate;
-  @Autowired
-  private InnReachTransactionRepository repository;
 
 
-  @Nested
-  class FilterByCreatedDateTest {
+  @ParameterizedTest
+  @MethodSource("auditDateTestData")
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-inn-reach-transactions.sql")
+  void returnTransactions_when_filteredByCreatedDate(List<OffsetDateTime> dates, FilterDateOperation operation,
+      List<String> expectedTrxTrackingIds) {
+    var responseEntity = testRestTemplate.getForEntity(constructUri("createdDate", dates, operation),
+        InnReachTransactionsDTO.class);
 
-    @BeforeEach
-    void changeCreatedDate() {
-      var transactions = repository.findAll();
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(expectedTrxTrackingIds.size(), responseEntity.getBody().getTotalRecords());
 
-      var trx1 = findTrxByTrackingId(transactions, PRE_POPULATED_TRACKING_ID1);
-      trx1.setCreatedDate(TARGET_DATE);
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(expectedTrxTrackingIds.size(), transactions.size());
 
-      var trx2 = findTrxByTrackingId(transactions, PRE_POPULATED_TRACKING_ID2);
-      trx2.setCreatedDate(TARGET_DATE.minusMonths(1));
-
-      var trx3 = findTrxByTrackingId(transactions, PRE_POPULATED_TRACKING_ID3);
-      trx3.setCreatedDate(TARGET_DATE.plusMonths(1));
-
-      repository.saveAll(transactions);
-    }
-
-    @ParameterizedTest
-    @MethodSource("org.folio.innreach.controller.InnReachTransactionDateFilterTest#auditDateTestData")
-    void returnTransactions_when_FilteredByCreatedDate(List<OffsetDateTime> dates, FilterDateOperation operation,
-        List<String> expectedTrxTrackingIds) {
-      var responseEntity = testRestTemplate.getForEntity(constructUri("createdDate", dates, operation),
-          InnReachTransactionsDTO.class);
-
-      assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-      assertNotNull(responseEntity.getBody());
-      assertEquals(expectedTrxTrackingIds.size(), responseEntity.getBody().getTotalRecords());
-
-      var transactions = responseEntity.getBody().getTransactions();
-      assertEquals(expectedTrxTrackingIds.size(), transactions.size());
-
-      var trackingIds = mapItems(transactions, InnReachTransactionDTO::getTrackingId);
-      assertThat(trackingIds, containsInAnyOrder(expectedTrxTrackingIds.toArray()));
-    }
-
+    var trackingIds = mapItems(transactions, InnReachTransactionDTO::getTrackingId);
+    assertThat(trackingIds, containsInAnyOrder(expectedTrxTrackingIds.toArray()));
   }
 
-  @Nested
-  class FilterByUpdatedDateTest {
-
-    @BeforeEach
-    void changeUpdatedDate() {
-      var transactions = repository.findAll();
-
-      var trx1 = findTrxByTrackingId(transactions, PRE_POPULATED_TRACKING_ID1);
-      trx1.setUpdatedDate(TARGET_DATE);
-
-      var trx2 = findTrxByTrackingId(transactions, PRE_POPULATED_TRACKING_ID2);
-      trx2.setUpdatedDate(TARGET_DATE.minusMonths(1));
-
-      var trx3 = findTrxByTrackingId(transactions, PRE_POPULATED_TRACKING_ID3);
-      trx3.setUpdatedDate(TARGET_DATE.plusMonths(1));
-
-      repository.saveAll(transactions);
-    }
-
-    @ParameterizedTest
-    @MethodSource("org.folio.innreach.controller.InnReachTransactionDateFilterTest#auditDateTestData")
-    void returnTransactions_when_FilteredByUpdatedDate(List<OffsetDateTime> dates, FilterDateOperation operation,
-        List<String> expectedTrxTrackingIds) {
-      var trx = repository.findAll();
-
-      trx.forEach(t -> System.out.printf("Transaction: tracking id = %s, updated date = %s%n",
-          t.getTrackingId(), t.getUpdatedDate().format(ISO_OFFSET_DATE_TIME)));
-
-      var responseEntity = testRestTemplate.getForEntity(constructUri("updatedDate", dates, operation),
+  @Test
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-inn-reach-transactions.sql")
+  void returnTransaction_when_filteredByCreatedDate_and_otherField() {
+    var responseEntity = testRestTemplate.getForEntity(
+          constructUri("createdDate", List.of(TARGET_DATE), equal) + "&type=PATRON",
           InnReachTransactionsDTO.class);
 
-      assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-      assertNotNull(responseEntity.getBody());
-      assertEquals(expectedTrxTrackingIds.size(), responseEntity.getBody().getTotalRecords());
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(1, responseEntity.getBody().getTotalRecords());
 
-      var transactions = responseEntity.getBody().getTransactions();
-      assertEquals(expectedTrxTrackingIds.size(), transactions.size());
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(1, transactions.size());
 
-      var trackingIds = mapItems(transactions, InnReachTransactionDTO::getTrackingId);
-      assertThat(trackingIds, containsInAnyOrder(expectedTrxTrackingIds.toArray()));
-    }
+    assertEquals(PRE_POPULATED_TRACKING_ID1, transactions.get(0).getTrackingId());
+  }
 
+  @ParameterizedTest
+  @MethodSource("auditDateTestData")
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-inn-reach-transactions.sql")
+  void returnTransactions_when_filteredByUpdatedDate(List<OffsetDateTime> dates, FilterDateOperation operation,
+      List<String> expectedTrxTrackingIds) {
+    var responseEntity = testRestTemplate.getForEntity(constructUri("updatedDate", dates, operation),
+        InnReachTransactionsDTO.class);
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(expectedTrxTrackingIds.size(), responseEntity.getBody().getTotalRecords());
+
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(expectedTrxTrackingIds.size(), transactions.size());
+
+    var trackingIds = mapItems(transactions, InnReachTransactionDTO::getTrackingId);
+    assertThat(trackingIds, containsInAnyOrder(expectedTrxTrackingIds.toArray()));
+  }
+
+  @Test
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-inn-reach-transactions.sql")
+  void returnTransaction_when_filteredByUpdatedDate_and_otherField() {
+    var responseEntity = testRestTemplate.getForEntity(
+        constructUri("updatedDate", List.of(TARGET_DATE), equal) + "&state=PATRON_HOLD",
+        InnReachTransactionsDTO.class);
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(1, responseEntity.getBody().getTotalRecords());
+
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(1, transactions.size());
+
+    assertEquals(PRE_POPULATED_TRACKING_ID1, transactions.get(0).getTrackingId());
+  }
+
+  @ParameterizedTest
+  @MethodSource("auditDateTestData")
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-hold-transactions.sql")
+  void returnTransactions_when_filteredByHoldCreatedDate(List<OffsetDateTime> dates, FilterDateOperation operation,
+      List<String> expectedTrxTrackingIds) {
+    var responseEntity = testRestTemplate.getForEntity(
+        constructUri("holdCreatedDate", dates, operation),
+        InnReachTransactionsDTO.class);
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(expectedTrxTrackingIds.size(), responseEntity.getBody().getTotalRecords());
+
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(expectedTrxTrackingIds.size(), transactions.size());
+
+    var trackingIds = mapItems(transactions, InnReachTransactionDTO::getTrackingId);
+    assertThat(trackingIds, containsInAnyOrder(expectedTrxTrackingIds.toArray()));
+  }
+
+  @Test
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-hold-transactions.sql")
+  void returnTransaction_when_filteredByHoldCreatedDate_and_otherField() {
+    var responseEntity = testRestTemplate.getForEntity(
+        constructUri("holdCreatedDate", List.of(TARGET_DATE), equal) + "&patronAgencyCode=qwe12",
+        InnReachTransactionsDTO.class);
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(1, responseEntity.getBody().getTotalRecords());
+
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(1, transactions.size());
+
+    assertEquals(PRE_POPULATED_TRACKING_ID1, transactions.get(0).getTrackingId());
+  }
+
+  @ParameterizedTest
+  @MethodSource("auditDateTestData")
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-hold-transactions.sql")
+  void returnTransactions_when_filteredByHoldUpdatedDate(List<OffsetDateTime> dates, FilterDateOperation operation,
+      List<String> expectedTrxTrackingIds) {
+    var responseEntity = testRestTemplate.getForEntity(
+        constructUri("holdUpdatedDate", dates, operation),
+        InnReachTransactionsDTO.class);
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(expectedTrxTrackingIds.size(), responseEntity.getBody().getTotalRecords());
+
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(expectedTrxTrackingIds.size(), transactions.size());
+
+    var trackingIds = mapItems(transactions, InnReachTransactionDTO::getTrackingId);
+    assertThat(trackingIds, containsInAnyOrder(expectedTrxTrackingIds.toArray()));
+  }
+
+  @Test
+  @Sql(scripts = "classpath:db/inn-reach-transaction/set-auditing-dates-for-hold-transactions.sql")
+  void returnTransaction_when_filteredByHoldUpdatedDate_and_otherField() {
+    var responseEntity = testRestTemplate.getForEntity(
+        constructUri("holdUpdatedDate", List.of(TARGET_DATE), equal) + "&patronAgencyCode=qwe12",
+        InnReachTransactionsDTO.class);
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody());
+    assertEquals(1, responseEntity.getBody().getTotalRecords());
+
+    var transactions = responseEntity.getBody().getTransactions();
+    assertEquals(1, transactions.size());
+
+    assertEquals(PRE_POPULATED_TRACKING_ID1, transactions.get(0).getTrackingId());
   }
 
   static Stream<Arguments> auditDateTestData() {
@@ -164,16 +223,12 @@ public class InnReachTransactionDateFilterTest extends BaseControllerTest {
         arguments(List.of(TARGET_DATE), greater, List.of(PRE_POPULATED_TRACKING_ID3)),
         arguments(List.of(TARGET_DATE), greaterOrEqual, List.of(PRE_POPULATED_TRACKING_ID1, PRE_POPULATED_TRACKING_ID3)),
         arguments(List.of(TARGET_DATE.minusDays(1), TARGET_DATE.plusDays(1)), between, List.of(PRE_POPULATED_TRACKING_ID1)),
-        arguments(List.of(TARGET_DATE), null, List.of(PRE_POPULATED_TRACKING_ID1)) // special case when operation is omitted -- treated as "equal"
+        // special case when operation is omitted -- treated as "equal"
+        arguments(List.of(TARGET_DATE), null, List.of(PRE_POPULATED_TRACKING_ID1)),
+        // special case when dates are not present -- date condition is not constructed, all records returned
+        arguments(Collections.emptyList(), less,
+            List.of(PRE_POPULATED_TRACKING_ID1, PRE_POPULATED_TRACKING_ID2, PRE_POPULATED_TRACKING_ID3))
     );
-  }
-
-  private static InnReachTransaction findTrxByTrackingId(List<InnReachTransaction> transactions,
-      String trackingId) {
-    return transactions.stream()
-        .filter(trx -> trx.getTrackingId().equals(trackingId))
-        .findFirst()
-        .orElseThrow();
   }
 
   private static String constructUri(String dateParam, List<OffsetDateTime> dates, FilterDateOperation operation) {
