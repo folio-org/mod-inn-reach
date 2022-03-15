34a649dd186f87961e72941a52fa563c81b7d0b1
aleksandr2110 <48136117+aleksandr2110@users.noreply.github.com>
2022-03-15 10:03:12 +0200

    [MODINREACH-216] Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record (#213)
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Added sql file.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Tests.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Added to PatronHoldDTO.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Added patron name to the specification.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test added.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test added.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Fix report.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Updated.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test added.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Deleted patron name and central patron type.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test added.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Tests added.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Tests fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Tests.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Tests fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. CirculationApiTest fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. OptimisticLockingTest fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. OwnerRenewCirculationApiTest fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Test fixed.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record.
    
    * MODINREACH-216 Add Patron Name and Patron Type (INN-Reach) to Patron Hold Transaction Record. Alter transaction hold.

--- src/main/resources/db/changelog/changes/v1.1.0/sql/2022-02-17-01__alter-transaction-hold-tables.sql
+++ src/main/resources/db/changelog/changes/v1.1.0/sql/2022-02-17-01__alter-transaction-hold-tables.sql
@@ -1,5 +1,5 @@
 ALTER TABLE transaction_hold
-  ADD COLUMN central_patron_type SMALLINT NOT NULL CHECK(central_patron_type BETWEEN 0 AND 255),
+  ADD COLUMN central_patron_type SMALLINT DEFAULT 0 NOT NULL CHECK(central_patron_type BETWEEN 0 AND 255),
   ADD COLUMN patron_name VARCHAR(255);
 
 UPDATE transaction_hold SET central_patron_type = item_hold.central_patron_type, patron_name = item_hold.patron_name
