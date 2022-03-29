018c9c363c2ac2a972d2b4052464bf8bc5c5968a
Alherd_Nestsiarkow <s1mple@tut.by>
2022-03-29 11:52:43 +0300

    added saving transaction before reporting

--- src/main/java/org/folio/innreach/domain/service/impl/InnReachTransactionActionServiceImpl.java
+++ src/main/java/org/folio/innreach/domain/service/impl/InnReachTransactionActionServiceImpl.java
@@ -324,6 +324,8 @@ public class InnReachTransactionActionServiceImpl implements InnReachTransaction
 
       transaction.setState(FINAL_CHECKIN);
 
+      transaction = saveInNewDbTransaction(transaction);
+
       notifier.reportFinalCheckIn(transaction);
 
       clearCentralPatronInfo(hold);
