40ae61c43a2f74203cc9592af6383582eec0831f
Oleksandr Oliinyk <oleksandr_oliinyk1@epam.com>
2022-03-31 11:47:10 +0300

    Fix bib info endpoint HTTP method

--- src/main/java/org/folio/innreach/controller/d2ir/BibInfoController.java
+++ src/main/java/org/folio/innreach/controller/d2ir/BibInfoController.java
@@ -6,8 +6,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class BibInfoController implements BibInfoD2irApi {
   private final BibInfoService bibInfoService;
 
   @Override
-  @GetMapping(value = "/getbibrecord/{bibId}/{centralCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/getbibrecord/{bibId}/{centralCode}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<BibInfoResponseDTO> getBibRecord(@PathVariable("bibId") String bibId,
                                                          @PathVariable("centralCode") String centralCode) {
     var info = bibInfoService.getBibInfo(bibId, centralCode);
