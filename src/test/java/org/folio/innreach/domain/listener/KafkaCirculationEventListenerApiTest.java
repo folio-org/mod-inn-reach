ffa705c372621227c36168c29dd575b7c01753a9
oleksandr_oliinyk2@epam.com <oleksandr_oliinyk2@epam.com>
2022-05-13 19:47:00 +0300

    MODINREACH-280 When Moving a Local Hold Request from One Item to Another, Update Local Hold Transaction to TRANSFER State. Test fixed.

--- src/test/java/org/folio/innreach/domain/listener/KafkaCirculationEventListenerApiTest.java
+++ src/test/java/org/folio/innreach/domain/listener/KafkaCirculationEventListenerApiTest.java
@@ -398,7 +398,7 @@ class KafkaCirculationEventListenerApiTest extends BaseKafkaApiTest {
 
     verify(eventProcessor).process(anyList(), any(Consumer.class));
 
-    verify(transactionRepository, times(1)).fetchActiveByRequestId(any());
+    verify(transactionRepository, times(2)).fetchActiveByRequestId(any());
     verify(inventoryClient, times(0)).findItem(any());
     verify(innReachExternalService, times(0)).postInnReachApi(any(), any(), any());
   }
