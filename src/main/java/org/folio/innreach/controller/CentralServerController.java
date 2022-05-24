d33445364ce6a275879c1818d0af5f4636552766
Alherd_Nestsiarkow <s1mple@tut.by>
2022-05-24 14:15:37 +0300

    test

---
+++ src/main/java/org/folio/innreach/controller/CentralServerController.java
@@ -0,0 +1,69 @@
+package org.folio.innreach.controller;
+
+import java.util.UUID;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.folio.innreach.domain.service.CentralServerService;
+import org.folio.innreach.dto.CentralServerDTO;
+import org.folio.innreach.dto.CentralServersDTO;
+import org.folio.innreach.rest.resource.CentralServersApi;
+
+@RequiredArgsConstructor
+@RestController
+@Validated
+@RequestMapping("/inn-reach/central-servers")
+public class CentralServerController implements CentralServersApi {
+
+  private final CentralServerService centralServerService;
+
+  @Override
+  @DeleteMapping("/{centralServerId}")
+  public ResponseEntity<Void> deleteCentralServer(@PathVariable UUID centralServerId) {
+    centralServerService.deleteCentralServer(centralServerId);
+    return ResponseEntity.noContent().build();
+  }
+
+  @Override
+  @GetMapping("/{centralServerId}")
+  public ResponseEntity<CentralServerDTO> getCentralServerById(@PathVariable UUID centralServerId) {
+    var centralServer = centralServerService.getCentralServer(centralServerId);
+    return ResponseEntity.ok(centralServer);
+  }
+
+  @Override
+  @GetMapping
+  public ResponseEntity<CentralServersDTO> getCentralServers(@Min(0) @Max(2147483647) @Valid Integer offset,
+                                                             @Min(0) @Max(2147483647) @Valid Integer limit) {
+    var allCentralServers = centralServerService.getAllCentralServers(offset, limit);
+    return ResponseEntity.ok(allCentralServers);
+  }
+
+  @Override
+  @PostMapping
+  public ResponseEntity<CentralServerDTO> postCentralServer(@Valid CentralServerDTO centralServerDTO) {
+    var createdCentralServer = centralServerService.createCentralServer(centralServerDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdCentralServer);
+  }
+
+  @Override
+  @PutMapping("/{centralServerId}")
+  public ResponseEntity<Void> updateCentralServer(@PathVariable UUID centralServerId, @Valid CentralServerDTO centralServerDTO) {
+    centralServerService.updateCentralServer(centralServerId, centralServerDTO);
+    return ResponseEntity.noContent().build();
+  }
+}
