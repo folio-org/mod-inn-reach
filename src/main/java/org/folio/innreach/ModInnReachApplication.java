fb3e9259b0169d06b92260d90847077564270d95
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2022-06-14 20:33:09 +0300

    versions update

--- src/main/java/org/folio/innreach/ModInnReachApplication.java
+++ src/main/java/org/folio/innreach/ModInnReachApplication.java
@@ -6,10 +6,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableFeignClients
 @EnableAsync
 @EnableRetry
