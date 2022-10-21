8b97286d02dc352b820016c72f11a33e2ac10bff
Arghya22 <Arghya_Mitra@epam.com>
2022-10-21 13:01:13 +0530

    [MODINREACH-316] 1. cleanup 2. logging modified

--- src/main/java/org/folio/innreach/domain/service/impl/VirtualRecordServiceImpl.java
+++ src/main/java/org/folio/innreach/domain/service/impl/VirtualRecordServiceImpl.java
@@ -2,7 +2,11 @@ package org.folio.innreach.domain.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.folio.innreach.domain.service.*;
+import org.folio.innreach.domain.service.HoldingsService;
+import org.folio.innreach.domain.service.InstanceService;
+import org.folio.innreach.domain.service.ItemService;
+import org.folio.innreach.domain.service.LoanService;
+import org.folio.innreach.domain.service.VirtualRecordService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,10 +23,11 @@ public class VirtualRecordServiceImpl implements VirtualRecordService {
   private final LoanService loanService;
   @Override
   public void deleteVirtualRecords(UUID folioItemId, UUID folioHoldingId, UUID folioInstanceId, UUID folioLoanId) {
-    log.info("Inside deleteVirtualRecords, loanId->>"+folioLoanId);
+    log.debug("deleteVirtualRecords :: parameters folioItemId:{} folioHoldingId:{} folioInstanceId:{} folioLoanId:{}",folioItemId,folioHoldingId,folioInstanceId,folioLoanId);
     Optional.ofNullable(folioItemId).ifPresent(itemService::delete);
     Optional.ofNullable(folioHoldingId).ifPresent(holdingsService::delete);
     Optional.ofNullable(folioInstanceId).ifPresent(instanceService::delete);
     Optional.ofNullable(folioLoanId).ifPresent(loanService::delete);
+    log.info("VirtualRecords Deleted");
   }
 }
