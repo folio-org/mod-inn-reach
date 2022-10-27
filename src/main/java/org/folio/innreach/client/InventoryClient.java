98f74d5fdcc60bb450dca965dcd691e31d784708
Arghya22 <Arghya_Mitra@epam.com>
2022-10-27 19:53:35 +0530

    [MODINREACH-316] 1. import fixed

--- src/main/java/org/folio/innreach/client/InventoryClient.java
+++ src/main/java/org/folio/innreach/client/InventoryClient.java
@@ -4,7 +4,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import org.folio.innreach.client.config.InventoryFeignClientConfig;
 import org.folio.innreach.domain.dto.folio.ResultList;
