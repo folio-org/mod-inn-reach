cb92b36271aeaa89cb73e10ebeea36563d31700d
Arin_Suryavanshi <arin_suryavanshi@epam.com>
2023-02-15 10:45:38 +0530

    [MODINREACH-347] : initial contribution updated

--- src/test/scripts/initial-contribution/reset-test.sh
+++
@@ -1,2 +0,0 @@
-TEST=$1
-./bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group folio-mod-innreach-contribution-events-group --topic folio.contrib.tester."$TEST":0 --to-offset 0 --reset-offsets --execute
--- src/test/scripts/initial-contribution/start-init-contrib-job.sh
+++
@@ -1,1 +0,0 @@
-curl -XPOST -HX-okapi-tenant:any  http://localhost:8081/inn-reach/central-servers/2d776536-9045-11ed-a1eb-0242ac120002/contributions
