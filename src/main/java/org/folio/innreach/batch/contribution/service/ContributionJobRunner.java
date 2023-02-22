e1b31dbc5f4ad11abe64c8ba67ff1ee66522cf76
Arin_Suryavanshi <arin_suryavanshi@epam.com>
2023-02-22 16:46:12 +0530

    [MODINREACH-347]: unit tests updated for initial contribution

--- src/main/java/org/folio/innreach/batch/contribution/service/ContributionJobRunner.java
+++ src/main/java/org/folio/innreach/batch/contribution/service/ContributionJobRunner.java
@@ -96,7 +96,7 @@ public class ContributionJobRunner {
   }
 
   public void runInitialContribution(ContributionJobContext context, InstanceIterationEvent event, Statistics stats, String topic) {
-    recordsProcessed.put(context.getTenantId(), recordsProcessed.get(context.getTenantId())+1);
+    recordsProcessed.put(context.getTenantId(), recordsProcessed.get(context.getTenantId()) == null ? 1 : recordsProcessed.get(context.getTenantId())+1);
 
     var contributionId = context.getContributionId();
 
