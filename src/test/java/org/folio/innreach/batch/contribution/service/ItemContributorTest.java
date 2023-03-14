47ce1270122e03e9bb8f4eefaed69207577f79b0
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-03-14 16:37:06 +0530

    [MODINREACH-347] : Added test case for recordContributionService

--- src/test/java/org/folio/innreach/batch/contribution/service/ItemContributorTest.java
+++ src/test/java/org/folio/innreach/batch/contribution/service/ItemContributorTest.java
@@ -1,12 +1,12 @@
 package org.folio.innreach.batch.contribution.service;
 
 import static com.google.common.collect.ImmutableList.of;
-import static org.folio.innreach.external.dto.InnReachResponse.*;
 import static org.folio.innreach.fixture.ContributionFixture.createInstance;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -20,10 +20,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.folio.innreach.external.dto.InnReachResponse;
+import org.folio.innreach.external.exception.InnReachConnectionException;
 import org.folio.innreach.external.exception.ServiceSuspendedException;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -81,11 +81,27 @@ class ItemContributorTest {
     service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem()));
 
     verify(irContributionService).contributeBibItems(eq(JOB_CONTEXT.getCentralServerId()), any(), any());
+
+    when(response.getErrors()).thenReturn(null);
+    service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem()));
+    verify(irContributionService,times(2)).contributeBibItems(eq(JOB_CONTEXT.getCentralServerId()), any(), any());
+
+
+    when(response.getErrors()).thenReturn(List.of());
+    service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem()));
+    verify(irContributionService,times(3)).contributeBibItems(eq(JOB_CONTEXT.getCentralServerId()), any(), any());
+
+    response = null;
+    service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem()));
+    verify(irContributionService,times(4)).contributeBibItems(eq(JOB_CONTEXT.getCentralServerId()), any(), any());
+
   }
   @Test
   void shouldContributeItems_throwException() throws SocketTimeoutException {
     InnReachResponse.Error errorResp1 = InnReachResponse.Error.builder().reason("Contribution to d2irm is not currently suspended").build();
     InnReachResponse.Error errorResp2 = InnReachResponse.Error.builder().reason("Contribution to d2irm is currently suspended").build();
+    InnReachResponse.Error errorResp3 = InnReachResponse.Error.builder().reason("Central Error").central("d2irm")
+                        .messages(Arrays.asList("connections allowed from this server")).build();
 
     when(irContributionService.contributeBibItems(any(), any(), any())).thenReturn(response);
     when(recordTransformationService.getBibItems(any(), any(), any())).thenReturn(List.of(new BibItem(),new BibItem()));
@@ -97,6 +113,10 @@ class ItemContributorTest {
 
     when(response.getErrors()).thenReturn(Arrays.asList(errorResp2));
     Assert.assertThrows(ServiceSuspendedException.class,()->service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem())));
+
+    when(response.getErrors()).thenReturn(Arrays.asList(errorResp3));
+    Assert.assertThrows(InnReachConnectionException.class,()->service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem())));
+
 
     when(recordTransformationService.getBibItems(any(), any(), any())).thenReturn(List.of());
     Assert.assertThrows(IllegalArgumentException.class,()->service.contributeItems(JOB_CONTEXT.getCentralServerId(), "test", of(createItem())));
