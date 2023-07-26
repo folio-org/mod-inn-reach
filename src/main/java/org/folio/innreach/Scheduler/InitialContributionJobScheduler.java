7245cb9218849afdb51fa4bfa4a141e8930645b7
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-07-26 14:16:41 +0530

    MODINREACH-380 Adding cache config to store the tenant details

--- src/main/java/org/folio/innreach/Scheduler/InitialContributionJobScheduler.java
+++ src/main/java/org/folio/innreach/Scheduler/InitialContributionJobScheduler.java
@@ -2,6 +2,7 @@ package org.folio.innreach.Scheduler;
 
 import com.google.common.cache.Cache;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.innreach.batch.contribution.service.ContributionJobRunner;
 import org.folio.innreach.domain.entity.TenantInfo;
@@ -19,13 +20,12 @@ import java.util.List;
 @AllArgsConstructor
 @Log4j2
 public class InitialContributionJobScheduler {
-  private TenantScopedExecutionService executionService;
-  private JobExecutionStatusRepository jobExecutionStatusRepository;
-  private ContributionJobRunner contributionJobRunner;
-  //private List<String> tenants;
-  private TenantInfoRepository tenantRepository;
+  private final TenantScopedExecutionService executionService;
+  private final JobExecutionStatusRepository jobExecutionStatusRepository;
+  private final ContributionJobRunner contributionJobRunner;
+  private final TenantInfoRepository tenantRepository;
   @Value(value = "${initial-contribution.fetch-limit}")
-  private int limit;
+  private final int limit;
   private final Cache<String, List<String>>  tenantDetailsCache;
   public List<String> loadTenants() {
     String tenantCacheKey = "tenantList";
@@ -43,7 +43,7 @@ public class InitialContributionJobScheduler {
   @Scheduled(fixedDelayString = "${initial-contribution.scheduler.fixed-delay}", initialDelayString = "${initial-contribution.scheduler.initial-delay}")
   public void processInitialContributionEvents() {
     List<String> tenants = loadTenants();
-    log.info("processInitialContributionEvents :: tenantsList {} ", tenants);
+    log.info("processInitialContributionEvents :: tenantsList {} , fetch limit {} ", tenants, limit);
     tenants.forEach(tenant ->
       executionService.runTenantScoped(tenant,
         () -> {
