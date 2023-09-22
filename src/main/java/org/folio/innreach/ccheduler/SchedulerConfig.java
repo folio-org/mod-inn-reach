1e0924393c487d6cbbdd4ea3bc306dab6c2dae45
pavankumar181 <pavankumar_shivashetty@epam.com>
2023-09-22 18:58:36 +0530

    MODINREACH-397 Added scheduler config class

--- src/main/java/org/folio/innreach/ccheduler/SchedulerConfig.java
+++ src/main/java/org/folio/innreach/ccheduler/SchedulerConfig.java
@@ -3,12 +3,14 @@ package org.folio.innreach.ccheduler;
 import lombok.extern.log4j.Log4j2;
 import org.folio.innreach.repository.JobExecutionStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @Service
 @Log4j2
+@Lazy
 public class SchedulerConfig {
   @Autowired
   private JobExecutionStatusRepository jobExecutionStatusRepository;
@@ -16,6 +18,11 @@ public class SchedulerConfig {
   @PostConstruct
   public void initialize(){
     log.info("InitialContributionJobScheduler:: initialize");
-    jobExecutionStatusRepository.updateJobExecutionRecordsByStatus();
+    try{
+      jobExecutionStatusRepository.updateJobExecutionRecordsByStatus();
+    }
+    catch (Exception ex){
+      log.info("exception ",ex);
+    }
   }
 }
