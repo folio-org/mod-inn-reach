50dba489c100dcc45c0c4b40fa4f677065d2fb09
pavankumar181 <pavankumar_shivashetty@epam.com>
2023-09-25 13:10:53 +0530

    MODINREACH-397 Added ExecutionContext

--- src/main/java/org/folio/innreach/ccheduler/SchedulerConfig.java
+++ src/main/java/org/folio/innreach/ccheduler/SchedulerConfig.java
@@ -4,9 +4,12 @@ import com.google.common.cache.Cache;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.innreach.domain.entity.TenantInfo;
+import org.folio.innreach.domain.service.impl.FolioExecutionContextBuilder;
 import org.folio.innreach.domain.service.impl.TenantScopedExecutionService;
 import org.folio.innreach.repository.JobExecutionStatusRepository;
 import org.folio.innreach.repository.TenantInfoRepository;
+import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.scope.FolioExecutionContextSetter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,26 +21,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SchedulerConfig {
   private final TenantScopedExecutionService executionService;
-  private JobExecutionStatusRepository jobExecutionStatusRepository;
+  private final JobExecutionStatusRepository jobExecutionStatusRepository;
   private final Cache<String, List<String>> tenantDetailsCache;
   private final TenantInfoRepository tenantRepository;
+  private final FolioExecutionContextBuilder folioExecutionContextBuilder;
+  private final FolioExecutionContext folioContext;
+
 
 
   @PostConstruct
   public void initialize(){
     log.info("InitialContributionJobScheduler:: initialize");
-    List<String> tenants = loadTenants();
-    log.info("processInitialContributionEvents :: tenantsList {}", tenants);
-    tenants.forEach(tenant ->
-      executionService.runTenantScoped(tenant,
-        () -> {
-          try {
-            jobExecutionStatusRepository.updateJobExecutionRecordsByStatus();
-          } catch (Exception ex) {
-            log.warn("Exception caught while processing Initial contribution for tenant {} {} ", tenant, ex.getMessage());
-          }
-        }
-      ));
+//    List<String> tenants = loadTenants();
+//    log.info("processInitialContributionEvents :: tenantsList {}", tenants);
+//    tenants.forEach(tenant ->
+//      executionService.runTenantScoped(tenant,
+//        () -> {
+//          try {
+//          } catch (Exception ex) {
+//            log.warn("Exception caught while processing Initial contribution for tenant {} {} ", tenant, ex.getMessage());
+//          }
+//        }
+//      ));
+    try (var context = new FolioExecutionContextSetter(folioExecutionContextBuilder.withUserId(folioContext,null))) {
+      jobExecutionStatusRepository.updateJobExecutionRecordsByStatus();
+    }
   }
   private List<String> loadTenants() {
     String tenantCacheKey = "tenantList";
