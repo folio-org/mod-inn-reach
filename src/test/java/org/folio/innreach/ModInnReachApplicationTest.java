c5b93fee63412ae3204b60e5613e597c0922d767
pavankumar181 <pavankumar_shivashetty@epam.com>
2023-10-19 16:45:38 +0530

    MODINREACH-403 Added Test case

---
+++ src/test/java/org/folio/innreach/ModInnReachApplicationTest.java
@@ -0,0 +1,17 @@
+package org.folio.innreach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
+
+public class ModInnReachApplicationTest {
+
+    @Test
+    void exceptionOnMissingSystemUserPassword() {
+        var e = assertThrows(IllegalArgumentException.class, () -> ModInnReachApplication.main(null));
+        assertThat(e.getMessage(), containsString(ModInnReachApplication.SYSTEM_USER_PASSWORD));
+    }
+
+}
