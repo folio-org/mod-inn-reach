cd412ea135f60ad8b247052f3bf6b933f85bf442
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-11-10 13:52:04 +0530

    FAT-9221 Fixing Transaction issue

--- src/main/java/org/folio/innreach/repository/TenantInfoRepository.java
+++ src/main/java/org/folio/innreach/repository/TenantInfoRepository.java
@@ -3,6 +3,7 @@ package org.folio.innreach.repository;
 import org.folio.innreach.domain.entity.TenantInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 @Repository
@@ -10,6 +11,7 @@ public interface TenantInfoRepository extends JpaRepository<TenantInfo, UUID> {
 
   TenantInfo findByTenantId(String id);
 
+  @Transactional
   void deleteByTenantId(String id);
 
 }
