512595698c9566b41945150e802b9dc1976d9da0
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2024-05-22 16:07:15 +0530

    MODINREACH-430 Adding new table to persist item contribution events

--- src/main/java/org/folio/innreach/domain/entity/Contribution.java
+++ src/main/java/org/folio/innreach/domain/entity/Contribution.java
@@ -90,9 +90,6 @@ public class Contribution extends Auditable implements Identifiable<UUID> {
   @Column(name = "job_id")
   private UUID jobId;
 
-  @OneToMany(cascade = CascadeType.ALL, mappedBy = "contribution", fetch = FetchType.LAZY)
-  private List<OngoingContributionStatus> ongoingContributionStatuses = new ArrayList<>();
-
   @AllArgsConstructor
   public enum Status {
     IN_PROGRESS("In Progress"),
