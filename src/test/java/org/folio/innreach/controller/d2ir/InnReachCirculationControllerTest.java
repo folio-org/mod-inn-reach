1a7fade23d4932be08fbca149391fc0b66672f1a
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-09-06 13:10:07 +0530

    MODINREACH-436 reproducing the issue - 5

--- src/test/java/org/folio/innreach/controller/d2ir/InnReachCirculationControllerTest.java
+++ src/test/java/org/folio/innreach/controller/d2ir/InnReachCirculationControllerTest.java
@@ -359,7 +359,7 @@ class InnReachCirculationControllerTest extends BaseControllerTest {
     assertEquals(BORROWING_SITE_CANCEL, transactionUpdated.getState());
   }
 
-//  reproducing failed test case 4
+//  reproducing failed test case 5
   @Test
   @Sql(scripts = {
     "classpath:db/central-server/pre-populate-central-server.sql",
