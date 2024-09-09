020fc2eaf1e72e92fbe4a696361db34c1ecb989f
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-09-09 15:17:19 +0530

    MODINREACH-436 reproducing the issue - trying clearing tables -9

--- src/test/java/org/folio/innreach/controller/d2ir/InnReachCirculationControllerTest.java
+++ src/test/java/org/folio/innreach/controller/d2ir/InnReachCirculationControllerTest.java
@@ -445,7 +445,6 @@ class InnReachCirculationControllerTest extends BaseControllerTest {
     assertNotEquals(RECEIVE_UNANNOUNCED, transactionUpdated.getState());
   }
 
-
   @Test
   @Sql(scripts = {
     "classpath:db/central-server/pre-populate-central-server.sql",
