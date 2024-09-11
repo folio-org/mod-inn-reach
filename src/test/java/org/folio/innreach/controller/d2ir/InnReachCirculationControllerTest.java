f9285fefafbf53fad73bef1e9869b563ad908fac
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-09-11 08:50:12 +0530

    MODINREACH-436 reproducing the issue with Isolation of test 6

--- src/test/java/org/folio/innreach/controller/d2ir/InnReachCirculationControllerTest.java
+++ src/test/java/org/folio/innreach/controller/d2ir/InnReachCirculationControllerTest.java
@@ -439,6 +439,7 @@ class InnReachCirculationControllerTest extends BaseControllerTest {
     assertNotEquals(RECEIVE_UNANNOUNCED, transactionUpdated.getState());
   }
 
+
   @Test
   @Sql(scripts = {
     "classpath:db/central-server/pre-populate-central-server.sql",
