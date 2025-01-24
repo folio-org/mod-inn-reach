a7bbd59bdef8a78ec35e43afcd524a9b75a5bf95
gurleenkaurbp <gurleen.kaur2505@gmail.com>
2025-01-24 19:13:06 +0530

    MODINREACH-468 - Another NPE handled

--- src/main/java/org/folio/innreach/domain/service/impl/AgencyMappingServiceImpl.java
+++ src/main/java/org/folio/innreach/domain/service/impl/AgencyMappingServiceImpl.java
@@ -97,10 +97,16 @@ public class AgencyMappingServiceImpl implements AgencyMappingService {
     var localServer = getLocalServerByAgencyCode(centralServerId, agencyCode);
     var localCode = localServer.getLocalCode();
 
-    return mapping.getLocalServers()
-      .stream()
-      .filter(m -> localCode.equals(m.getLocalCode()))
+    return mapping.getLocalServers().stream()
+      .filter(server -> localCode.equals(server.getLocalCode()))
       .map(AgencyLocationLscMappingDTO::getLocationId)
+      .map(locationId -> {
+        if (locationId == null) {
+          log.warn("getLocationIdByLocalServer:: locationId is null in localServer with localCode: {}", localCode);
+        }
+        return locationId;
+      })
+      .filter(Objects::nonNull)
       .findFirst();
   }
 
