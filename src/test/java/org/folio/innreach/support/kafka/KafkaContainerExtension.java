fd232537a396ff21eb06f1e70a287637364b337c
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2026-07-03 17:30:35 +0500

    feat: add container extensions and annotations for unified test infrastructure

---
+++ src/test/java/org/folio/innreach/support/kafka/KafkaContainerExtension.java
@@ -0,0 +1,85 @@
+package org.folio.innreach.support.kafka;
+
+import static org.testcontainers.utility.DockerImageName.parse;
+
+import java.util.List;
+import java.util.Properties;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Log4j2
+public class KafkaContainerExtension implements BeforeAllCallback, AfterAllCallback {
+
+  private static final String SPRING_PROPERTY_NAME = "spring.kafka.bootstrap-servers";
+  private static final DockerImageName KAFKA_IMAGE = parse("apache/kafka-native:3.8.0");
+  private static final KafkaContainer CONTAINER = new KafkaContainer(KAFKA_IMAGE)
+    .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "false")
+    .withStartupAttempts(3);
+
+  @Override
+  public void beforeAll(ExtensionContext context) {
+    if (!CONTAINER.isRunning()) {
+      CONTAINER.start();
+    }
+
+    System.setProperty(SPRING_PROPERTY_NAME, CONTAINER.getBootstrapServers());
+  }
+
+  @Override
+  public void afterAll(ExtensionContext context) {
+    System.clearProperty(SPRING_PROPERTY_NAME);
+  }
+
+  @SneakyThrows
+  public static void createTopics(List<String> topicNames) {
+    var newTopics = topicNames.stream()
+      .map(topicName -> new NewTopic(topicName, 1, (short) 1))
+      .toList();
+
+    log.info("Creating topics: {}", newTopics);
+    try (var adminClient = getAdminClient()) {
+      adminClient.createTopics(newTopics);
+    }
+  }
+
+  @SneakyThrows
+  public static void deleteTopics(List<String> topicNames) {
+    try (var adminClient = getAdminClient()) {
+      var existingTopics = adminClient.listTopics().names().get();
+      var topicsToDelete = topicNames.stream()
+        .filter(existingTopics::contains)
+        .toList();
+
+      if (!topicsToDelete.isEmpty()) {
+        log.info("Removing Kafka topics: {}", topicsToDelete);
+        adminClient.deleteTopics(topicsToDelete);
+      } else {
+        log.debug("No Kafka topics to delete from: {}", topicNames);
+      }
+    }
+  }
+
+  public static AdminClient getAdminClient() {
+    var props = new Properties();
+    props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, getBootstrapServers());
+    props.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000");
+    return KafkaAdminClient.create(props);
+  }
+
+  public static String getBootstrapServers() {
+    if (!CONTAINER.isRunning()) {
+      log.error("Kafka container is not running. Returning empty bootstrap servers.");
+      return "";
+    }
+    return CONTAINER.getBootstrapServers();
+  }
+}
---
+++ src/test/java/org/folio/innreach/support/wiremock/WiremockStubExtension.java
@@ -0,0 +1,234 @@
+package org.folio.innreach.support.wiremock;
+
+import static com.github.tomakehurst.wiremock.admin.model.ServeEventQuery.ALL_UNMATCHED;
+import static org.apache.commons.lang3.StringUtils.trimToNull;
+import static org.folio.innreach.support.wiremock.WiremockContainerExtension.getWireMockAdminClient;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import com.github.tomakehurst.wiremock.verification.LoggedRequest;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+
+public class WiremockStubExtension implements
+  BeforeEachCallback, AfterEachCallback, BeforeAllCallback, AfterAllCallback {
+
+  private static final String STUB_IDS = "stubIds";
+  private static final Namespace NAMESPACE = Namespace.create(WiremockStubExtension.class);
+  private static final ObjectMapper MAPPER = new ObjectMapper()
+    .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+    .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  private static final ClassLoader CLASS_LOADER = WiremockStubExtension.class.getClassLoader();
+
+  @Override
+  public void afterAll(ExtensionContext context) {
+    resetWiremockStubs();
+  }
+
+  @Override
+  public void beforeAll(ExtensionContext context) {
+    resetWiremockStubs();
+  }
+
+  @Override
+  public void beforeEach(ExtensionContext context) {
+    var testMethod = context.getRequiredTestMethod();
+    var testClass = context.getRequiredTestClass();
+
+    var stubFilePaths = new LinkedHashSet<String>();
+    stubFilePaths.addAll(getStubPathsSafe(testClass.getAnnotation(WireMockStub.class)));
+    stubFilePaths.addAll(getStubPathsSafe(testMethod.getAnnotation(WireMockStub.class)));
+
+    var stubIds = new ArrayList<StubMappingRepr>();
+    context.getStore(NAMESPACE).put(getStubIdsKey(context), stubIds);
+    for (@Language("file-reference") var stubPath : stubFilePaths) {
+      stubIds.addAll(addStubMapping(stubPath));
+    }
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+    var store = context.getStore(NAMESPACE);
+    @SuppressWarnings("unchecked")
+    var definedStubMappings = (List<StubMappingRepr>) store.get(getStubIdsKey(context));
+    var unmatchedRequestValues = findUnmatchedRequests();
+
+    if (definedStubMappings == null || definedStubMappings.isEmpty()) {
+      store.remove(getStubIdsKey(context));
+      resetWiremockStubs();
+
+      validateUnmatchedRequests(unmatchedRequestValues);
+      return;
+    }
+
+    var unusedStubs = findUnusedStubs(definedStubMappings);
+
+    store.remove(getStubIdsKey(context));
+    resetWiremockStubs();
+
+    validateUnmatchedRequests(unmatchedRequestValues);
+    validateUnusedStubMappings(unusedStubs);
+  }
+
+  public static void resetWiremockStubs() {
+    getWireMockAdminClient().resetAll();
+  }
+
+  public static void addStubMappings(@Language("file-reference") String... paths) {
+    for (@Language("file-reference") var path : paths) {
+      addStubMapping(path);
+    }
+  }
+
+  public static List<StubMappingRepr> addStubMapping(@Language("file-reference") String path) {
+    if (StringUtils.isBlank(path)) {
+      throw new IllegalArgumentException("Path must not be empty");
+    }
+
+    var classpathPrefix = "classpath:";
+    var resourcePath = StringUtils.startsWith(path, classpathPrefix)
+      ? path.substring(classpathPrefix.length())
+      : path;
+
+    var wireMockAdminClient = getWireMockAdminClient();
+    var jsonString = readJsonString(resourcePath);
+    var stubMappings = parseStubMappings(jsonString);
+    var stubIds = new ArrayList<StubMappingRepr>();
+    for (var stubMapping : stubMappings) {
+      var stubId = UUID.randomUUID();
+      stubMapping.setId(stubId);
+      stubIds.add(new StubMappingRepr(stubId, getStubMappingStringRepresentation(stubMapping)));
+      wireMockAdminClient.addStubMapping(stubMapping);
+    }
+
+    return stubIds;
+  }
+
+  private static String getStubIdsKey(ExtensionContext context) {
+    return context.getRequiredTestMethod().getName() + "-" + STUB_IDS;
+  }
+
+  @SneakyThrows
+  private static String readJsonString(String path) {
+    var updatedPath = path.startsWith("/") ? path.substring(1) : path;
+    try (var stream = CLASS_LOADER.getResourceAsStream(updatedPath)) {
+      assertNotNull(stream, "Resource not found: " + updatedPath);
+      return new String(stream.readAllBytes());
+    }
+  }
+
+  @SneakyThrows
+  private static List<StubMapping> parseStubMappings(String jsonString) {
+    var jsonNode = MAPPER.readTree(jsonString);
+
+    var mappingsKey = "mappings";
+    var mappingNode = jsonNode.path(mappingsKey);
+    if (mappingNode instanceof ArrayNode arrMappingNode) {
+      return arrMappingNode.valueStream()
+        .map(arrayValue -> StubMapping.buildFrom(arrayValue.toString()))
+        .toList();
+    }
+
+    return List.of(StubMapping.buildFrom(jsonString));
+  }
+
+  @SneakyThrows
+  private static String getServeEventRequestString(LoggedRequest request) {
+    var map = new LinkedHashMap<>();
+    map.put("method", request.getMethod().getName());
+    map.put("path", URI.create(request.getUrl()).getPath());
+    map.put("queryParams", request.getQueryParams().toString());
+    map.put("requestBody", trimToNull(request.getBodyAsString()));
+    return MAPPER.writeValueAsString(map);
+  }
+
+  @SneakyThrows
+  private static String getStubMappingStringRepresentation(StubMapping stubMapping) {
+    var request = stubMapping.getRequest();
+    var map = new HashMap<String, Object>();
+    map.put("method", request.getMethod().getName());
+    map.put("url", request.getUrl());
+    map.put("urlPath", request.getUrlPath());
+    map.put("urlPattern", request.getUrlPattern());
+    map.put("urlPathTemplate", request.getUrlPathTemplate());
+    return MAPPER.writeValueAsString(map);
+  }
+
+  private static List<String> getStubPathsSafe(WireMockStub stubs) {
+    return Optional.ofNullable(stubs)
+      .map(WireMockStub::value)
+      .stream()
+      .flatMap(Arrays::stream)
+      .distinct()
+      .toList();
+  }
+
+  private static List<String> findUnusedStubs(List<StubMappingRepr> definedStubMappings) {
+    if (definedStubMappings == null || definedStubMappings.isEmpty()) {
+      return List.of();
+    }
+
+    var allServeEvents = getWireMockAdminClient().getServeEvents();
+    var matchedStubEventsIds = allServeEvents.getServeEvents().stream()
+      .map(ServeEvent::getStubMapping)
+      .map(StubMapping::getId)
+      .filter(Objects::nonNull)
+      .toList();
+
+    return definedStubMappings.stream()
+      .filter(mapping -> !matchedStubEventsIds.contains(mapping.id()))
+      .map(StubMappingRepr::representation)
+      .toList();
+  }
+
+  private static List<String> findUnmatchedRequests() {
+    var unmatchedServeEvents = getWireMockAdminClient().getServeEvents(ALL_UNMATCHED);
+    return unmatchedServeEvents.getServeEvents().stream()
+      .map(event -> getServeEventRequestString(event.getRequest()))
+      .toList();
+  }
+
+  private static void validateUnusedStubMappings(List<String> unusedStubs) {
+    assertTrue(unusedStubs.isEmpty(), "The following stubs were never called:\n"
+      + formatRepresentation(unusedStubs));
+  }
+
+  private static void validateUnmatchedRequests(List<String> unmatchedRequestValues) {
+    assertTrue(unmatchedRequestValues.isEmpty(), "Should be 0 unmatched requests:\n"
+      + formatRepresentation(unmatchedRequestValues));
+  }
+
+  private static String formatRepresentation(List<String> unmatchedStubRepresentations) {
+    return unmatchedStubRepresentations.stream()
+      .map(" => %s"::formatted)
+      .collect(Collectors.joining("\n", "[\n", "\n]"));
+  }
+
+  public record StubMappingRepr(UUID id, String representation) {
+  }
+}
