a82a00a50743c6bf06f782d7044f9147d9333758
Wayne Schneider <wayne@indexdata.com>
2026-07-28 17:05:37 -0500

    Update the InventoryViewClient to use new instanceHrid index in inventory-view interface 3.3
    Towards MODINREACH-607

--- src/main/java/org/folio/innreach/client/InventoryViewClient.java
+++ src/main/java/org/folio/innreach/client/InventoryViewClient.java
@@ -21,10 +21,10 @@ import org.folio.innreach.dto.Item;
 @FeignClient(name = "inventory-view", configuration = FolioFeignClientConfig.class)
 public interface InventoryViewClient {
 
-  @GetMapping(path = "/instances?query=(id={instanceId})&limit=1", consumes = "binary/octet-stream")
+  @GetMapping(path = "/instances?query=(id=={instanceId})&limit=1", consumes = "binary/octet-stream")
   ResultList<InstanceView> getInstanceById(@PathVariable("instanceId") UUID instanceId);
 
-  @GetMapping(path = "/instances?query=(instance.hrid={hrid})&limit=1", consumes = "binary/octet-stream")
+  @GetMapping(path = "/instances?query=(instanceHrid=={hrid})&limit=1", consumes = "binary/octet-stream")
   ResultList<InstanceView> getInstanceByHrid(@PathVariable("hrid") String instanceHrid);
 
   @Data
